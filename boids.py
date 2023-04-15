2d0ec5cda460f121fb4252adb272e06628c30209
Tristan Saldanha <tristan@timebyping.com>
2023-04-14 18:11:06 -0700

    buttery smooth

--- boids.py
+++ boids.py
@@ -14,10 +14,13 @@ HEIGHT = 1080
 size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(size)
 
-WEIGHT_ALIGNMENT = .1
-WEIGHT_COHESION = .01
-WEIGHT_SEPARATION = .1
+WEIGHT_ALIGNMENT = .5
+WEIGHT_COHESION = .3
+WEIGHT_SEPARATION = 1
+WEIGHT_JITTER = .001
+
 SPEED_LIMIT = 3
+INERTIA = .9
 
 pygame.display.set_caption("Boids")
 
@@ -26,18 +29,30 @@ class Boid:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.dx = random.uniform(-1, 1) * 10
-        self.dy = random.uniform(-1, 1) * 10
+        self.dx = random.uniform(-1, 1) * 2
+        self.dy = SPEED_LIMIT - (self.dx ** 2) ** .5 * random.choice([-1, 1])
+        
+        
         self.rgb = (random.randrange(256), random.randrange(256), random.randrange(256))
 
     def update(self, flock):
         # Compute the average velocity of nearby boids
+        old_dx = self.dx
+        old_dy = self.dy
         
         nearby_boids = [b for b in flock if b != self and math.dist((self.x, self.y), (b.x, b.y)) < 100]
+        closer_boids = [b for b in flock if b != self and math.dist((self.x, self.y), (b.x, b.y)) < 50]
+        
+        self.dx = 0
+        self.dy = 0
+        
+        # remove closer boids from nearby boids
+        for b in closer_boids:
+            nearby_boids.remove(b)
+        
         if nearby_boids:
             avg_dx = sum(b.dx for b in nearby_boids) / len(nearby_boids)
             avg_dy = sum(b.dy for b in nearby_boids) / len(nearby_boids)
-            # moves 1/8th of the way towards the average velocity
             self.dx += (avg_dx - self.dx) * WEIGHT_ALIGNMENT
             self.dy += (avg_dy - self.dy) * WEIGHT_ALIGNMENT
 
@@ -50,18 +65,34 @@ class Boid:
             self.dy += (avg_y - self.y) * WEIGHT_COHESION
 
         # boids inside 50 pixel radius will move away from each other
-        nearby_boids = [b for b in flock if b != self and math.dist((self.x, self.y), (b.x, b.y)) < 50]
-        if nearby_boids:
-            for b in nearby_boids:
-                self.dx -= (b.x - self.x) * WEIGHT_SEPARATION
-                self.dy -= (b.y - self.y) * WEIGHT_SEPARATION
+        if closer_boids:
+            avg_x = sum(b.x for b in closer_boids) / len(closer_boids)
+            avg_y = sum(b.y for b in closer_boids) / len(closer_boids)
+            self.dx += (self.x - avg_x) * WEIGHT_SEPARATION
+            self.dy += (self.y - avg_y) * WEIGHT_SEPARATION
 
+        # normalize the velocity
+        if self.dx != 0 and self.dy != 0:
+            speed = math.sqrt(self.dx ** 2 + self.dy ** 2)
+            self.dx *= SPEED_LIMIT / speed
+            self.dy *= SPEED_LIMIT / speed
+        else:
+            self.dx = old_dx
+            self.dy = old_dy
+
+        # Jitter should be after normalization! took a while to fund that bug
+        self.dx += random.normalvariate(-1, 1) * WEIGHT_JITTER
+        self.dy += random.normalvariate(-1, 1) * WEIGHT_JITTER
+
+        self.dx = ((old_dx * INERTIA) + (self.dx * (1 - INERTIA)))
+        self.dy = ((old_dy * INERTIA) + (self.dy * (1 - INERTIA)))
+        
         # normalize the velocity
         speed = math.sqrt(self.dx ** 2 + self.dy ** 2)
         self.dx *= SPEED_LIMIT / speed
         self.dy *= SPEED_LIMIT / speed
 
-        # Update the boid's position
+        # Update the boid's position, but have turning be smooth    
         self.x += self.dx
         self.y += self.dy
 
@@ -80,7 +111,7 @@ class Boid:
         angle = math.atan2(self.dy, self.dx)
 
         # Compute the vertices of the quadrilateral
-        length = 20  # Length of the arrow
+        length = 12  # Length of the arrow
         width = 8  # Width of the arrow
         x1, y1 = self.x + length * math.cos(angle), self.y + length * math.sin(angle)
         x2, y2 = self.x + width * math.cos(angle + 3 * math.pi / 4), self.y + width * math.sin(angle + 3 * math.pi / 4)
